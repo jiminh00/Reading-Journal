9bf0899262599c70841465582d23b0912e4c9444
JIMIN HWANG <hwang0010@naver.com>
2025-01-24 13:26:54 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/jimin/readingjournal/ReadingJournalApplication.java
@@ -0,0 +1,13 @@
+package com.jimin.readingjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jimin/readingjournal/ReadingJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimin.readingjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
