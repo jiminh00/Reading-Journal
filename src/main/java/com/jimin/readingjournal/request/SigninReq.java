2cebe4a190abbe58dd7b3e7002d5d74e40c19ae9
JIMIN HWANG <hwang0010@naver.com>
2025-01-31 21:45:20 +0900

    feat: 로그인, 로그아웃 컨트롤러, 서비스 구현 #7

---
+++ src/main/java/com/jimin/readingjournal/request/SigninReq.java
@@ -0,0 +1,11 @@
+package com.jimin.readingjournal.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SigninReq {
+    private String id;
+    private String password;
+}
--- src/main/java/com/jimin/readingjournal/service/AuthService.java
+++ src/main/java/com/jimin/readingjournal/service/AuthService.java
@@ -1,7 +1,11 @@
 package com.jimin.readingjournal.service;
 
+import com.jimin.readingjournal.request.SigninReq;
 import com.jimin.readingjournal.request.SignupReq;
+import jakarta.servlet.http.HttpSession;
 
 public interface AuthService {
     void signup(SignupReq signupReq);
+
+    boolean signin(SigninReq signinReq, HttpSession session);
 }
