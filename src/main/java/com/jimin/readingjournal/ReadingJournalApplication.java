0dff71e0cab39db9625bf0a9251e7dc7bd2a3d6c
JIMIN HWANG <hwang0010@naver.com>
2025-02-01 14:29:20 +0900

    fix: MapperScan 수정
    - 프로젝트 구조 변경으로 인해 Mapper 패키지 변경됨.
    - 이에 따른 MapperScan 값 수정.

--- src/main/java/com/jimin/readingjournal/ReadingJournalApplication.java
+++ src/main/java/com/jimin/readingjournal/ReadingJournalApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.jimin.readingjournal.mapper")
+@MapperScan({"com.jimin.readingjournal.domain.auth.mapper", "com.jimin.readingjournal.domain.journal.mapper"})
 public class ReadingJournalApplication {
 
 	public static void main(String[] args) {
